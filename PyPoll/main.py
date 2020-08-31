a461d725f87168a9e66189696abd336a8bde4364
rchaynie <reidhaynie@gmail.com>
2020-08-31 19:38:14 +0000

    fixed win percentage

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,12 +7,16 @@ path=os.path.join("Resources","election_data.csv")
 
 
 vote_total=0
-correy_list=[]
-correy_vote=0
+
 khan_list=[]
 khan_vote=0
+
+correy_list=[]
+correy_vote=0
+
 li_list=[]
 li_vote=0
+
 otooley_list=[]
 otooley_vote=0
 
@@ -30,24 +34,6 @@ with open(path, "r") as file:
             otooley_list.append("Candidate")
             
     
-#     for row in dict_reader:
-#         if row["Candidate"]=="Correy":
-#             correy_list.append("Candidate")
-    
-#     for row in dict_reader:
-#         if row["Candidate"]=="khan":
-#             khan_list.append("Candidate")
-            
-#     for row in dict_reader:
-#         if row["Candidate"]=="li":
-#             li_list.append("Candidate")
-    
-#     for row in dict_reader:
-#         if row["Candidate"]=="O'Tooley":
-#             otooley_list.append("Candidate")
-    
-    
-    
 for row in correy_list:
     correy_vote+=1
 
@@ -60,13 +46,20 @@ for row in li_list:
 for row in otooley_list:
     otooley_vote+=1
     
+# variables for win percentage by candidate
+
+khan_per=format((khan_vote/vote_total)*100,".3f")
+correy_per=format((correy_vote/vote_total)*100,".3f")
+li_per=format((li_vote/vote_total)*100,".3f")
+otooley_per=format((otooley_vote/vote_total)*100,".3f") 
+
 print("Election Results \n --------------------------")
 print(f"Total Votes: {vote_total}")
 print("--------------------------")
-print(f"Khan: {(khan_vote/vote_total)*100}% ({khan_vote})")    
-print(f"Correy: {(correy_vote/vote_total)*100}% ({correy_vote})")
-print(f"Li: {(li_vote/vote_total)*100}% ({li_vote})")
-print(f"O'Tooley: {(otooley_vote/vote_total)*100}% ({otooley_vote})")
+print(f"Khan: {khan_per}% ({khan_vote})")    
+print(f"Correy: {correy_per}% ({correy_vote})")
+print(f"Li: {li_per}% ({li_vote})")
+print(f"O'Tooley: {otooley_per}% ({otooley_vote})")
 print("--------------------------")
 print(f"Winner: ______")
     
