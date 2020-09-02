a4a159ed4ea1de0fb4979be8d621c652f36d9bbd
rchaynie <reidhaynie@gmail.com>
2020-09-02 14:33:03 +0000

    working on average change

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,6 +7,11 @@ unique_months=[]
 total_months = 0
 total_prof = 0
 
+change_by_month=[]
+avg_change=0
+max_change=0
+
+
 
 # finding total months
 with open (path, "r") as file:
@@ -18,21 +23,29 @@ with open (path, "r") as file:
            
     for e in unique_months:
         total_months+=1
+    
+    
         
 # finding total profit 
 with open (path, "r") as file:
     dict_reader=csv.DictReader(file)
-       
+          
     for row in dict_reader:
         total_prof+= int(row['Profit/Losses'])
 
             
+    for row in dict_reader:
+        print(row)
+        break
+            
+            
+# Average change by month         
+    for i in dict_reader:
+        change_by_month.append(dict_reader[i]-dict_reader[i-1])
 
-            
-            
-            
-            
-            
+print(change_by_month)
+    
+        
 print(f"Total Months: {total_months}")      
 print(f"Total: ${total_prof}")
 
